460856d518f72113d835911aeb0d976f45245446
huangjingyang <huangjingyang@mozat.com>
2020-05-12 10:29:24 +0800

    feat: 配置es6语法

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import "./assets/scss/main.scss";
 import "./assets/css/style.css";
 
+
 new Vue({
     el: "#app",
     render: (h) => h(App),
 });
+
+
+let abc = () => {
+    console.log("this is hahhahahahaha");
+};
+
+console.log(abc)
